0af5b0c07acf89e38839bc876cb622ec6b6b7c6c
Sajan Mittal <sajanmittal@hotmail.com>
2020-05-01 20:17:40 +0530

    Floating Label Material Date Picker for Xamarin Forms.

---
+++ FloatingLabelDatePicker/FloatingLabelDatePicker.Android/FloatingLabelDatePickerRenderer.cs
@@ -0,0 +1,54 @@
+﻿using Android.Content;
+using Android.Widget;
+using FloatingLabelDatePicker.Droid;
+using Xamarin.Forms;
+using Xamarin.Forms.Internals;
+using Xamarin.Forms.Material.Android;
+using Xamarin.Forms.Platform.Android;
+using AColor = Android.Graphics.Color;
+using XDatePicker = Xamarin.Forms.DatePicker;
+
+[assembly: ExportRenderer(typeof(FloatingLabelDatePicker.FloatingLabelDatePicker), typeof(FloatingLabelDatePickerRenderer), new[] { typeof(VisualMarker.MaterialVisual) })]
+namespace FloatingLabelDatePicker.Droid
+{
+	public class FloatingLabelDatePickerRenderer : MaterialDatePickerRenderer
+	{
+		private MaterialPickerTextInputLayout _layout;
+
+		public FloatingLabelDatePickerRenderer(Context context) : base(context)
+		{
+		}
+
+		protected override MaterialPickerTextInputLayout CreateNativeControl()
+		{
+			_layout = base.CreateNativeControl();
+			return _layout;
+		}
+
+		protected override void OnElementChanged(ElementChangedEventArgs<XDatePicker> e)
+		{
+			base.OnElementChanged(e);
+			FloatingLabelDatePicker element = Element as FloatingLabelDatePicker;
+
+			if (!string.IsNullOrWhiteSpace(element.Title))
+			{
+				UpdateTitle(element.Title);
+			}
+		}
+
+		public void UpdateTitle(string hint)
+		{
+			SetHint(Element, hint);
+		}
+
+		private void SetHint(VisualElement element, string hint)
+		{
+			_layout.HintEnabled = !string.IsNullOrWhiteSpace(hint);
+			if (_layout.HintEnabled)
+			{
+				element?.InvalidateMeasureNonVirtual(InvalidationTrigger.VerticalOptionsChanged);
+				_layout.Hint = hint;
+			}
+		}
+	}
+}
---
+++ FloatingLabelDatePicker/FloatingLabelDatePicker/FloatingLabelDatePicker.cs
@@ -0,0 +1,17 @@
+﻿
+using Xamarin.Forms;
+
+namespace FloatingLabelDatePicker
+{
+	public class FloatingLabelDatePicker : DatePicker
+	{
+		public static readonly BindableProperty TitleProperty = BindableProperty.Create(propertyName: nameof(Title), returnType: typeof(string), declaringType: typeof(FloatingLabelDatePicker), defaultValue: default(string), defaultBindingMode: BindingMode.TwoWay);
+
+		public string Title
+		{
+			get => (string)GetValue(TitleProperty);
+			set => SetValue(TitleProperty, value);
+		}
+
+	}
+}
